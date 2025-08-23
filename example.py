1b53b213fb1eeb4c10dea246e6d9baebb358d219
Delanna Do <50529918+Ddelanna@users.noreply.github.com>
2025-08-22 19:30:27 -0500

    Update example.py

--- example.py
+++ example.py
@@ -1,15 +1,42 @@
-from SamplingHelperFunctions import KnnAccuracy, GraphMetricAccuracy
-from SamplingAlgorithms import ProbCoverSampling, ConnectedComponentSampling
-from CreateData import *
+import CreateData
+import HelperFunctions
+import PredictionMethods
+import SamplingAlgorithms
+from HelperFunctions import AdjacencyMatrices
 
 
+
+
+class Iterate:
+    def __init__(self, data_generation, sampling_method, prediction_method, random_state=None):
+        NUM_POINTS = 1000
+        BUDGET = 10
+        RADIUS = 1.5
+
+        unlabeled_points, oracle = data_generation(num_points=NUM_POINTS, random_state=random_state)
+
+        for adjacency_matrix in [AdjacencyMatrices().binary_epsilon_graph(unlabeled_points, RADIUS)]:
+            model = sampling_method(unlabeled_points, BUDGET, adjacency_matrix=adjacency_matrix, random_state=random_state)
+            score = prediction_method(model, oracle).score
+            print('score', score)
 
 if __name__ == '__main__':
+    # todo: metrics to use: euclidean, fermat (p=1,2,4?)
+    # todo: fix AdjacencyMatrices
+    # todo: switch sampling methods over to sparse / improve runtime
 
-    unlabeled_points, oracle = create_spiral_data(1000, dimension=7)
+    # todo: using MNIST, FashionMNIST, and CIFAR from jeff's graphlearning github
+    # todo: record runtimes for creating distance / adjacency matrices and for the algorithm
 
-    PC = ProbCoverSampling(unlabeled_points, budget=6, radius=1.5)
-    print('KNN Accuracy', KnnAccuracy(PC, oracle, create_plots=True).score)
+    Iterate(data_generation=CreateData.create_spiral_data,
+            sampling_method=SamplingAlgorithms.ConnectedComponentSampling,
+            prediction_method=PredictionMethods.GraphMetricAccuracy,
+            random_state=1)
 
-    CC = ConnectedComponentSampling(unlabeled_points, budget=6, alpha=0.95)
-    print('GraphMetric Accuracy', GraphMetricAccuracy(CC, oracle, create_plots=True).score)
+    distance_matrix = AdjacencyMatrices().distance_matrix(data)
+    SamplingAlgorithms.KmeansSampling(data, budget, distance_matrix)
+
+
+
+
+

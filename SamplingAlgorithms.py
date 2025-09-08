0d87bdace9baa8547bbfe38f3cf10f09bec081c4
Delanna Do <50529918+Ddelanna@users.noreply.github.com>
2025-09-08 00:42:44 -0400

    Update SamplingAlgorithms.py

--- SamplingAlgorithms.py
+++ SamplingAlgorithms.py
@@ -3,56 +3,7 @@ os.environ['OMP_NUM_THREADS'] = '4'
 
 import heapq
 import numpy as np
-from HelperFunctions import AdjacencyMatrices, _set_random_state
-
-
-
-class KmeansSampling:
-    def __init__(self, unlabeled_points, budget, distance_matrix, distance_type='peikonal',
-                 n_local_trials=None, random_state=None):
-        self.unlabeled_points = unlabeled_points
-        self.budget = budget
-
-        if n_local_trials is None:
-            self._n_local_trials = 3 + int(np.log(budget))
-        else:
-            self._n_local_trials = n_local_trials
-
-        self._random_state = _set_random_state(random_state)
-        self.distance_matrix = distance_matrix
-        self._distance_type = distance_type
-
-        self.query_indices = self._get_query_indices()
-
-    def __find_best_sample_point(self, query_indices, dists):
-        rand_vals = self._random_state.uniform(size=self._n_local_trials) * np.sum(dists)
-        from sklearn.utils.extmath import stable_cumsum
-        candidates_samples = np.searchsorted(stable_cumsum(dists), rand_vals)
-        np.clip(candidates_samples, None, dists.size - 1, out=candidates_samples)
-
-        MSE_per_candidate =[]  # mean squared error
-        dists_per_candidate = []
-        for candidate_sample in candidates_samples:
-            boundary_set = query_indices + [candidate_sample] # combine candidate and previous centers
-            candidate_distance = AdjacencyMatrices().compute_distance(self.distance_matrix, boundary_set,
-                                                                      distance_type=self._distance_type)
-            MSE_per_candidate.append(np.sum(candidate_distance))
-            dists_per_candidate.append(candidate_distance)
-        min_MSE_idx = np.argmin(MSE_per_candidate)
-        best_sample_point = candidates_samples[min_MSE_idx]
-        dists = dists_per_candidate[min_MSE_idx] ** 2
-
-        return best_sample_point, dists
-
-    def _get_query_indices(self):
-        query_indices = [self._random_state.choice(self.unlabeled_points.index)] # initialize query_indices
-
-        dists = AdjacencyMatrices().compute_distance(self.distance_matrix, query_indices, distance_type=self._distance_type) ** 2
-        for idx in range(1, self.budget):
-            best_sample_point, dists = self.__find_best_sample_point(query_indices, dists)
-            query_indices.append(best_sample_point)
-
-        return query_indices
+from HelperFunctions import _set_random_state
 
 
 class RandomSampling:
@@ -62,6 +13,49 @@ class RandomSampling:
 
         _random_state = _set_random_state(random_state)
         self.query_indices = _random_state.choice(self.unlabeled_points.index, budget)
+
+
+class KmeansSampling:
+    def __init__(self, unlabeled_points, budget, graph_repr, random_state=None, n_local_trials=None):
+        self.unlabeled_points = unlabeled_points
+        self.budget = budget
+        self.graph = graph_repr
+
+        self._random_state = _set_random_state(random_state)
+        if n_local_trials is None:
+            self._n_local_trials = 3 + int(np.log(budget))
+        else:
+            self._n_local_trials = n_local_trials
+
+        self.query_indices = self._get_query_indices()
+
+    def __find_best_sample_point(self, query_indices, dists):
+        rand_vals = self._random_state.uniform(size=self._n_local_trials) * np.sum(dists)
+        from sklearn.utils.extmath import stable_cumsum
+        candidates_samples_indices = np.searchsorted(stable_cumsum(dists), rand_vals)
+        np.clip(candidates_samples_indices, None, dists.size-1, out=candidates_samples_indices)
+
+        MSE_per_candidate = []  # mean squared error per candidate
+        dists_per_candidate = []
+        for candidate_sample_idx in candidates_samples_indices:
+            candidate_distance = self.graph.dijkstra(bdy_set=query_indices + [candidate_sample_idx], bdy_val=0)
+            MSE_per_candidate.append(np.sum(candidate_distance))
+            dists_per_candidate.append(candidate_distance)
+        min_MSE_idx = np.argmin(MSE_per_candidate)
+        best_sample_point = candidates_samples_indices[min_MSE_idx]
+        dists = dists_per_candidate[min_MSE_idx]
+
+        return best_sample_point, dists
+
+    def _get_query_indices(self):
+        query_indices = [self._random_state.choice(self.unlabeled_points.shape[0])] # initialize query_indices
+
+        dists = self.graph.dijkstra(bdy_set=query_indices)
+        for _ in range(1, self.budget):
+            best_sample_point_idx, dists = self.__find_best_sample_point(query_indices, dists)
+            query_indices.append(best_sample_point_idx)
+
+        return [self.unlabeled_points.index[idx] for idx in query_indices]
 
 
 class ProbCoverSampling:
@@ -76,17 +70,19 @@ class ProbCoverSampling:
     def __update_edges(self, query_idx):
         """ multiplying the query_idx column by 0 so that edges.sum() has less weight in the
         vertices that share an edge with the query_idx vertex """
+
         # get the corresponding index on self.adjacency_matrix to the query index on self.unlabeled_points
         adjacency_query_idx = np.where(self.unlabeled_points.index == query_idx)[0][0]
 
-        # remove all outgoing edges of the labeled point AND all of its surrounding points
+        # get the indices of all the neighboring points to the query point (including the query point itself)
         from scipy.sparse import issparse
         if issparse(self.adjacency_matrix):
             neighboring_point_indices = self.adjacency_matrix.getcol(adjacency_query_idx).indices
-            self.adjacency_matrix[:, neighboring_point_indices] *= 0
         else:
             neighboring_point_indices = np.where(self.adjacency_matrix[adjacency_query_idx] == 1)
-            self.adjacency_matrix[:, neighboring_point_indices] *= 0
+
+        # remove all outgoing edges of the neighboring points
+        self.adjacency_matrix[:, neighboring_point_indices] *= 0
 
     def _get_query_indices(self):
         """ Iteratively picks the point with the highest number of outgoing edges
@@ -100,8 +96,12 @@ class ProbCoverSampling:
 
             # check if all data points have been removed from unlabeled pool
             if most_outgoing_edges == 0:
-                print('USER WARNING: Under-utilization of budget.')
-                query_idx = self._random_state.choice(np.setdiff1d(self.unlabeled_points.index, query_indices))
+                # print('USER WARNING: Under-utilization of budget.')
+                random_query_indices = self._random_state.choice(np.setdiff1d(self.unlabeled_points.index, query_indices),
+                                                      size=self.budget-len(query_indices),
+                                                      replace=False)
+                query_indices = np.concatenate((query_indices, random_query_indices))
+                return query_indices
 
             # add the index of the data point with the most out-going edges
             query_indices.append(query_idx)
@@ -149,7 +149,7 @@ class ConnectedComponentSampling:
         return component_budgets
 
     def _allot_component_budgets(self):
-        self.num_points = self.unlabeled_points.shape[0]
+        num_points = self.unlabeled_points.shape[0]
 
         # ensure all large components have budget >= 1
         component_budgets = self.__sample_from_largest_components()
@@ -158,11 +158,11 @@ class ConnectedComponentSampling:
         from math import floor
         remaining_budget = self.budget - sum(component_budgets)
         for idx in range(self.n_components):
-            component_budgets[idx] += floor(self.component_sizes[idx] / self.num_points * remaining_budget)
+            component_budgets[idx] += floor(self.component_sizes[idx] / num_points * remaining_budget)
 
         # if there is leftover budget, sample randomly proportional to size
         if sum(component_budgets) < self.budget:
-            distribution = [self.component_sizes[idx] / self.num_points for idx in range(self.n_components)]
+            distribution = [self.component_sizes[idx] / num_points for idx in range(self.n_components)]
             for _ in range(self.budget - sum(component_budgets)):
                 component_budgets[self._random_state.choice(np.arange(self.n_components), p=distribution)] += 1
 
@@ -174,13 +174,11 @@ class ConnectedComponentSampling:
         if query_indices is None:
             query_indices = ProbCoverSampling(component_data,
                                               budget=component_budget,
-                                              adjacency_matrix=sub_adjacency_matrix,
-                                              random_state=self._random_state).query_indices
+                                              adjacency_matrix=sub_adjacency_matrix).query_indices
         else:
             new_query_indices = ProbCoverSampling(component_data,
                                                   budget=component_budget,
-                                                  adjacency_matrix=sub_adjacency_matrix,
-                                                  random_state=self._random_state).query_indices
+                                                  adjacency_matrix=sub_adjacency_matrix).query_indices
             query_indices = np.append(query_indices, new_query_indices)
 
         return query_indices
@@ -202,6 +200,10 @@ class ConnectedComponentSampling:
         self.n_components, self.component_labels, self.component_sizes = self._find_connected_components()
         self.component_budgets = self._allot_component_budgets()
         self.query_indices = self._apply_probcover()
+
+
+
+
 
 
 
